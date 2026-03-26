b264ee8c55a99bd9c30a81dea06c4509fbf5a175
Irene <grim6ace@gmail.com>
2026-03-26 23:06:13 +0300

    ver 3

---
+++ src/js/general.js
@@ -0,0 +1,24 @@
+/****************
+  COPYRIGHT DATE
+*****************/
+/* 
+const copyrightDate = document.querySelector('.copyright-date');
+copyrightDate.innerHTML = new Date().getFullYear(); */
+
+/********************
+  BACK-TO-TOP button
+*********************/
+
+/* const backToTopBtn = document.querySelector('.back-to-top-button');
+
+function btnVisibleOnScroll() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    backToTopBtn.style.opacity = 1;
+    backToTopBtn.style.visibility = "visible";
+  } else {
+    backToTopBtn.style.opacity = 0;
+    backToTopBtn.style.visibility = "hidden";
+  }
+};
+
+window.addEventListener('scroll', btnVisibleOnScroll); */
