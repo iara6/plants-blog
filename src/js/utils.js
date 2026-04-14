3f970c831ce83262fa2b9b73416b3405a7d730bb
Irene <grim6ace@gmail.com>
2026-04-14 23:02:03 +0300

    ver 18

---
+++ src/js/utils.js
@@ -0,0 +1,46 @@
+export function slugify(text) {
+  return text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w-]+/g, '')
+    .replace(/--+/g, '-')
+    .replace(/^-+/, '')
+    .replace(/-+$/, '');
+}
+
+export function formatBlogPosts(posts, {
+  filterOutDrafts = true,
+  filterOutFuturePosts = true,
+  sortByDate = true, 
+  limit = undefined
+} = {}) {
+  
+  const filteredPosts = posts.reduce((acc, post) => {
+    const { date, draft } = post.frontmatter;
+
+    if (filterOutDrafts && draft) return acc; // will pass an empty array if both are true
+
+    if (filterOutFuturePosts && new Date(date) > new Date()) return acc;
+
+    // add post to acc
+    acc.push(post);
+
+    return acc;
+  }, [])
+
+  // sort by date or randomize
+  if (sortByDate) {
+    filteredPosts.sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date))
+  } else {
+    filteredPosts.sort(() => Math.random() - 0.5)
+  }
+
+  // limit if number is passed
+  if (typeof limit === "number") {
+    return filteredPosts.slice(0, limit);
+  }
+  
+  return filteredPosts;
+
+}
