e74c7233aec32d112dfef2aabb0d938a0c908a5e
Irene <grim6ace@gmail.com>
2026-04-22 22:45:42 +0300

    ver 26

--- src/js/general.js
+++ src/js/general.js
@@ -1,3 +1,30 @@
+/******************
+  Dark mode button 
+*******************/
+
+function initDarkMode() {
+  const switchBtn = document.querySelector('.switch-button');
+  if (!switchBtn) return;
+
+  let darkModeOn = JSON.parse(localStorage.getItem('mode')) || false;
+
+/*   if (darkModeOn) {
+    document.documentElement.classList.add('dark-mode');
+    switchBtn.classList.add('slide');
+  } */
+
+  switchBtn.addEventListener('click', () => {
+    darkModeOn = !darkModeOn;
+    document.documentElement.classList.toggle('dark-mode', darkModeOn);
+    switchBtn.classList.toggle('slide', darkModeOn);
+    localStorage.setItem('mode', JSON.stringify(darkModeOn));
+  });
+}
+// toggle('class', condition); documentElement returns <html>
+
+initDarkMode();
+document.addEventListener('astro:page-load', initDarkMode);
+
 /****************
   COPYRIGHT DATE
 *****************/
